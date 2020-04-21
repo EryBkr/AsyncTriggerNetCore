e1caabba894ce13b09f30a1f4130f185b1d7933c
Eray Bakır <info@eraybakir.com>
2020-04-21 11:19:28 +0300

    Test-1

---
+++ AsynTraining/TestAsyncTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynTraining
+{
+    public class TestAsyncTrigger : HostedService
+    {
+
+        protected override async Task ExecuteAsync(CancellationToken cToken)
+        {
+            while (!cToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Time : "+DateTime.Now);
+                await Task.Delay(TimeSpan.FromSeconds(10), cToken);
+            }
+        }
+    }
+}
